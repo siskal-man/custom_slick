449d469e6c249a67391c091b7d985327d7b4e7c8
siskal-man <block-code@mail.ru>
2021-12-05 16:50:31 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    $('.slider').slick({
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 300,
+        easing: 'ease',
+        infinite: false,
+        initialSlide: 0,
+        draggable: false,
+
+    });
 })
